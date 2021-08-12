3edd97f06bfa50827d4bdc6a108f4387b1c1f893
yCroma <kuroyuki0728@gmail.com>
2021-08-12 11:27:04 +0900

    UPDATE: syokika

--- opt/main.py
+++ opt/main.py
@@ -12,9 +12,13 @@ def main():
 # URLからデータを取得する
 def get_url_data():
     result = requests.get(URL)
-    data = result.text
-    return data
+    data = []
+    json = {}
+    # list de kakomanaito object ga detekuru
+    lines = list(map(lambda x: x.split("\t"),result.text.splitlines()))
 
+    # properties = ["ID", "start", "end", "transcription"]
+    properties = lines[0]
 
 if __name__ == "__main__":
     main()
